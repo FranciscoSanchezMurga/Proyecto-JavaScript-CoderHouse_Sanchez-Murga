c6c596f8553fb7d26151e952a497067c5a6a5713
Francisco Sanchez Murga <fransmurga@gmail.com>
2022-10-09 19:00:17 -0300

    modificacion nombre archivos y enlaces + función listarProductos (lista los productos basandose en el objeto productos)

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 let productos = [
     p1 = {
         id: 1,
-        nombre : "Sándwich de milanesa",
+        nombre: "Sándwich de milanesa",
         precio: 500,
     },
     p2 = {
         id: 2,
-        nombre : "Sándwich de lomito",
+        nombre: "Sándwich de lomito",
         precio: 600,
     },
     p3 = {
         id: 3,
-        nombre : "Pizza especial",
+        nombre: "Pizza especial",
         precio: 1000,
     },
 ];
@@ -24,13 +24,13 @@ let continuar;
 
 function continuarOTerminar(total) {
     continuar = prompt("Hasta ahora debería pagar $" + total + "\nSi desea continuar añadiendo productos presione 1, en caso contrario 2.");
-    while ((continuar!="1")&&(continuar!="2")) {
+    while ((continuar != "1") && (continuar != "2")) {
         continuar = prompt("Se equivocó de comando.\nSi desea continuar añadiendo productos presione 1, en caso contrario 2.");
     };
     return continuar;
 };
 
-function calcularCostoParcial(productoSeleccionado,cantidadSeleccionada) {
+function calcularCostoParcial(productoSeleccionado, cantidadSeleccionada) {
     switch (productoSeleccionado) {
         case "1":
             return PRECIO1 * cantidadSeleccionada;
@@ -41,13 +41,13 @@ function calcularCostoParcial(productoSeleccionado,cantidadSeleccionada) {
     }
 };
 
-function plasmarOperacionParcialEnConsola(productoSeleccionado,cantidadSeleccionada,costoParcial) {
+function plasmarOperacionParcialEnConsola(productoSeleccionado, cantidadSeleccionada, costoParcial) {
     console.log("Producto " + productoSeleccionado + " x " + cantidadSeleccionada + " = " + costoParcial);
 };
 
 function validarCantidad(cantidadSeleccionada) {
     let validezCantidad;
-    if ( isNaN(Number(cantidadSeleccionada)) || (cantidadSeleccionada % 1 != 0 ) ) {
+    if (isNaN(Number(cantidadSeleccionada)) || (cantidadSeleccionada % 1 != 0)) {
         validezCantidad = false;
     } else {
         validezCantidad = true;
@@ -65,10 +65,31 @@ function seleccionarCantidad() {
     return cantidadSeleccionada;
 };
 
+function listarProductos() {
+    let listaProductos = "";
+    for (let i = 0; i < productos.length; i += 1) {
+        if (i == productos.length) {
+            listaProductos += `${i + 1}) ${productos[i].nombre} - $${productos[i].precio}.`
+        } else {
+            listaProductos += `${i + 1}) ${productos[i].nombre} - $${productos[i].precio}.\n`;
+        }
+
+    };
+    return listaProductos;
+};
+
 function seleccionarProducto() {
-    let productoSeleccionado = prompt("SELECCIONE UN PRODUCTO\nTipee el número que le corresponda al item según esta lista:\n1) Sandwich de milanesa - $450.\n2) Sándwich de lomito - $550.\n3) Pizza especial - $1000");
-    while ((productoSeleccionado!="1")&&(productoSeleccionado!="2")&&(productoSeleccionado!="3")) {
-        productoSeleccionado = prompt("SELECCIONE UN PRODUCTO VÁLIDO!! Se equivocó de comando.\nTipee el número que le corresponda al item según esta lista:\n1) Sandwich de milanesa - $450.\n2) Sándwich de lomito - $550.\n3) Pizza especial - $1000");
+    let productoSeleccionado = prompt(
+        `SELECCIONE UN PRODUCTO
+Tipee el número que le corresponda al item según esta lista:
+${listarProductos()}`
+    );
+    while ((productoSeleccionado != "1") && (productoSeleccionado != "2") && (productoSeleccionado != "3")) {
+        productoSeleccionado = prompt(
+            `SELECCIONE UN PRODUCTO VÁLIDO!! Se equivocó de comando.
+Tipee el número que le corresponda al item según esta lista:
+${listarProductos()}`
+        );
     };
     return productoSeleccionado;
 };
@@ -77,12 +98,12 @@ function realizarPedido() {
     do {
         let productoSeleccionado = seleccionarProducto();
         let cantidadSeleccionada = seleccionarCantidad();
-        let costoParcial = calcularCostoParcial(productoSeleccionado,cantidadSeleccionada);
-        plasmarOperacionParcialEnConsola(productoSeleccionado,cantidadSeleccionada,costoParcial);
+        let costoParcial = calcularCostoParcial(productoSeleccionado, cantidadSeleccionada);
+        plasmarOperacionParcialEnConsola(productoSeleccionado, cantidadSeleccionada, costoParcial);
         total += costoParcial;
         continuar = continuarOTerminar(total);
     } while (continuar == "1");
-    console.log("TOTA A PAGAR: " + total );
+    console.log("TOTA A PAGAR: " + total);
 };
 
 realizarPedido();
