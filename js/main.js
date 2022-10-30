158b4c2c07815c0162f946ddef76b1aa514be2ad
Francisco Sanchez Murga <fransmurga@gmail.com>
2022-10-29 21:54:56 -0300

    Añado imágenes al objeto producto

---
+++ js/main.js
@@ -0,0 +1,20 @@
+let productos = [
+    p1 = {
+        id: 1,
+        nombre: "Sándwich de milanesa",
+        precio: 500,
+        imagen: "https://lh3.googleusercontent.com/lcn0osAXTRZedXLFntYvZFjHCXlKjVjt5fv-JAzRHt-dOubZ3oRhrbnxBcuDwboHMkT_MjqZTXzDdYqqVYs6pPpGq1aHxGamLCATNbCzqEmP34MHgdegnuxFD-7s3ktRLmp6HC5_Mg=w2400"
+    },
+    p2 = {
+        id: 2,
+        nombre: "Sándwich de lomito",
+        precio: 600,
+        imagen: "https://lh3.googleusercontent.com/hAClHNkCCYYkElrvYuWFo2A8nSpgC49jesphUrELgan92tjqb5EKyO_iX0-hiBqEDfA3l4LHqCsK4Ukk_Rb6llFibrkELTaiVYMGiGQuE39-PpZ-Ve1yH1kewEYHwy-iwFLL_jq-Wg=w2400" 
+    },
+    p3 = {
+        id: 3,
+        nombre: "Pizza especial",
+        precio: 1000,
+        imagen: "" 
+    },
+];
